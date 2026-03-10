a1cb1b6600890131747deec3249531fb8f5f3bf8
xelpaxu <axeljohn.nuqui@wvsu.edu.ph>
2026-03-10 17:56:53 +0800

    feat: modify main.tsx to set up correct routing and layout structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { RootLayout } from "./root-layout";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RootLayout />
-  </React.StrictMode>
-);
-
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <RootLayout />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
