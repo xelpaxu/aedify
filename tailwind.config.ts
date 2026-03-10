21ecf782187c1c60af574dddfaedce7ac3767150
xelpaxu <axeljohn.nuqui@wvsu.edu.ph>
2026-03-10 17:57:48 +0800

    update config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,22 @@ const config: Config = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
       colors: {
-        brand: {
-          500: "#22c55e"
+        primary: {
+          50: '#ecfeff',
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2', // Cyan
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+          950: '#083344',
         }
       }
     }
@@ -15,4 +28,3 @@ const config: Config = {
 };
 
 export default config;
-
